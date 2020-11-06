7a98c54c3a1e16a9fe8ccc78f788e0dc02587108
goteninwza007 <73677460+goteninwza007@users.noreply.github.com>
2020-11-06 16:58:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <html>
-    <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-5">
-                    <img src="viwe.jpg" style="height:750px; width:440px">
-                </div>
-                <div class="col-12 col-md-3"">
-                    <div class="row">
-                        <img src="viwe.jpg" style="height:250px; width: 500px" class="img-fluid">
-                        <img src="viwe.jpg" style="height:250px; width: 500px" class="img-fluid">
-                        <img src="viwe.jpg" style="height:250px; width: 500px" class="img-fluid">
-                    </div>
-                </div>
-                <div class="col-12 col-md-4"">
-                    <img src="viwe.jpg" style="height:375px; width: 500px" class="img-fluid">
-                    <img src="viwe.jpg" style="height:375px; width: 500px" class="img-fluid">
-                </div>
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
+<body>
+    <div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="row" style="height: 100%;">
+                <img src="viwe.jpg" class="img-fluid" style="height: 100%; width: 100%;">
+            </div>
+            </div>
+            <div class="col-sm-3">
+            <div class="row" style="height: 100%;">
+                <img src="viwe.jpg" class="img-fluid" style="height: 33.3%; width: 100%;">
+                <img src="viwe.jpg" class="img-fluid" style="height: 33.3%; width: 100%;">
+                <img src="viwe.jpg" class="img-fluid" style="height: 33.3%; width: 100%;">
+            </div>
+            </div>
+            <div class="col-sm-3">
+            <div class="row" style="height: 100%;">
+                <img src="viwe.jpg" class="img-fluid" style="height: 66.57%; width: 100%;">
+                <img src="viwe.jpg" class="img-fluid" style="height: 33.43%; width: 100%;">
             </div>
         </div>
-    </body>
+    </div>
+    </div>
+</body> 
 </html>
